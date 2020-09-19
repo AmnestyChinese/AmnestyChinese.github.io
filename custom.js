7c12027a6f8c9388b8f10f4844423053dbdfcfa5
EC2 Default User <ec2-user@ip-172-26-4-22.ap-southeast-1.compute.internal>
2020-09-19 16:58:40 +0800

    Commit at 2020-09-19 16:58:40

--- custom.js
+++ custom.js
@@ -1,3 +1,18 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-116298107-5');
+
+(function() {
+	var po = document.createElement('script');
+	po.type = 'text/javascript';
+	po.async = true;
+	po.src = '//www.googletagmanager.com/gtag/js?id=UA-116298107-5';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(po, s);
+})();
+
 jQuery(document).ready(function ($) {
 	// $('.amnesty_cookie_text').html('本网站为备份之用，如要浏览最新内容请前往<a href="https://zh.amnesty.org/">国际特赦组织中文网站</a>');
 	$('a[href*="wp-content/uploads/"][href*=".pdf"]').each(function() {
